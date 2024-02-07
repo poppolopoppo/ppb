d2672383d33024a25ea17d18d1f4e64b4c966998
poppolopoppo <phamtom.x@gmail.com>
2024-02-07 03:39:46 +0100

    Bump to 0.1.1 due to invalidation of Action binary format
    See new ActionAlias helper

--- utils/UtilsFingerprint.go
+++ utils/UtilsFingerprint.go
@@ -8,7 +8,7 @@ import (
 	"github.com/poppolopoppo/ppb/internal/base"
 )
 
-const PROCESS_VERSION = "0.1.0"
+const PROCESS_VERSION = "0.1.1"
 
 /***************************************
  * Process Fingerprint
